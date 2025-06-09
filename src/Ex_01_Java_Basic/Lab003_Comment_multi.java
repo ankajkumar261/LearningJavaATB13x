f6f9bf117eacbba2885b1239e59cb120e302a8af
ankajkumar261 <ankajkumar261@gmail.com>
2025-06-09 22:41:05 +0530

    New_Push

---
+++ src/Ex_01_Java_Basic/Lab003_Comment_multi.java
@@ -0,0 +1,12 @@
+package Ex_01_Java_Basic;
+
+public class Lab003_Comment_multi {
+    public static void main(String[] args) {
+
+        /*
+        hi all this is a multi line comments
+        hi
+         */
+        System.out.println("multiple comments ");
+    }
+}
