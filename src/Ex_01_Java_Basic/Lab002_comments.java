b15776401effd86a919eb14b25325476afb62e99
ankajkumar261 <ankajkumar261@gmail.com>
2025-06-09 07:20:01 +0530

    New_Push

---
+++ src/Ex_01_Java_Basic/Lab002_comments.java
@@ -0,0 +1,8 @@
+package Ex_01_Java_Basic;
+
+public class Lab002_comments {
+    public static void main(String[] args) {
+//        this line is not executable
+        System.out.println("hello");
+    }
+}
