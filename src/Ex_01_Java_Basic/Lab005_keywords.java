4155f2a5e8ab623a48abaf6f0e848f8906324607
ankajkumar261 <ankajkumar261@gmail.com>
2025-06-09 22:46:22 +0530

    New_Push

---
+++ src/Ex_01_Java_Basic/Lab005_keywords.java
@@ -0,0 +1,8 @@
+package Ex_01_Java_Basic;
+
+public class Lab005_keywords {
+    public static void main(String[] args) {
+        System.out.println("hi , ankaj");
+//        here package, public , class , static, void are keywords
+    }
+}
---
+++ src/Ex_01_Java_Basic/Lab006_identifier.java
@@ -0,0 +1,9 @@
+package Ex_01_Java_Basic;
+
+public class Lab006_identifier {
+    public static void main(String[] args) {
+        System.out.println("Hi ankaj");
+
+//        Lab006_identifier , Ex_01_Java_Basic are identifers
+    }
+}
