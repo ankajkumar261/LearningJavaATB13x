afa5458324b6d254b533738cdccccc42c2d48bb6
ankajkumar261 <ankajkumar261@gmail.com>
2025-06-10 22:44:27 +0530

    New_Push

---
+++ src/Ex_02_Java_Basic/Ex_02_java_Basic.java
@@ -0,0 +1,20 @@
+package Ex_02_Java_Basic;
+
+public class Ex_02_java_Basic {
+    public static void main(String[] args) {
+        System.out.println("table of 9");
+        int a=9;
+        System.out.printf("%d x %d = %d\n",a , 1 , a * 1);
+        System.out.printf("%d x %d = %d\n",a , 2 , a * 2);
+        System.out.printf("%d x %d = %d\n",a , 3 , a * 3);
+        System.out.printf("%d x %d = %d\n",a , 4 , a * 4);
+        System.out.printf("%d x %d = %d\n",a , 5 , a * 5);
+        System.out.printf("%d x %d = %d\n",a , 6 , a * 6);
+        System.out.printf("%d x %d = %d\n",a , 7 , a * 7);
+        System.out.printf("%d x %d = %d\n",a , 8 , a * 8);
+        System.out.printf("%d x %d = %d\n",a , 9 , a * 9);
+        System.out.printf("%d x %d = %d\n",a , 10 , a * 10);
+
+
+    }
+}
