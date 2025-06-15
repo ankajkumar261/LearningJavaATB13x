039c533e31388957373a40135fd4fc05d9b91771
ankajkumar261 <ankajkumar261@gmail.com>
2025-06-15 20:53:29 +0530

    New_Push

---
+++ src/Ex_06_ternaryOP/task_1.java
@@ -0,0 +1,10 @@
+package Ex_06_ternaryOP;
+
+public class task_1 {
+    public static void main(String[] args) {
+        int a = 400;
+        int b = 20 ;
+        int max = (a > b) ? a : b;
+        System.out.println(max);
+    }
+}
---
+++ src/Ex_06_ternaryOP/task_2.java
@@ -0,0 +1,9 @@
+package Ex_06_ternaryOP;
+
+public class task_2 {
+    public static void main(String[] args) {
+         int a = 5;
+         String oddoreven = (a%2 ==0) ? "even" : "odd";
+        System.out.println(oddoreven);
+    }
+}
