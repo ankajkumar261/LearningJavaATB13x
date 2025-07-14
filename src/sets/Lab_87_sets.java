05007666c1873b6658e51947806044564b8e3878
ankajk <Ankajkumar261@gmail.com>
2025-07-14 14:25:48 +0530

    new_push

---
+++ src/sets/Lab_87_sets.java
@@ -0,0 +1,17 @@
+package sets;
+
+import java.util.HashSet;
+
+public class Lab_87_sets {
+    public static void main(String[] args) {
+
+        HashSet hashSet = new HashSet();
+        hashSet.add(3);
+        hashSet.add(3);
+        hashSet.add("Ankaj");
+
+        System.out.println(hashSet);
+
+
+    }
+}
